197c5dda1f67ea4fded85e659375226e6f0521d2
Pratik <pratikkaple1nov2002@gmail.com>
2023-01-15 21:54:25 +0530

    Template Work made

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-var items = [];
+var items = ["Buy Food", "Make Food", "Eat Food"];
+var workList = [];
 
 app.get("/", function (req, res) {
     var today = new Date();
@@ -17,18 +18,36 @@ app.get("/", function (req, res) {
         day: 'numeric'
     };
 
-    var day = today.toLocaleDateString("us-GB", options);
+    var day = today.toLocaleDateString("us-US", options);
 
-    res.render("list", { Eday: day, newListItems: items });
+    res.render("list", { listTitle: day, newListItems: items });
+})
+
+app.get("/work", function (req, res) {
+    res.render("list", { listTitle: "Work List", newListItems: workList });
+})
+
+app.post("/work", function (req, res) {
+    workList.push(req.body.newItem);
+    res.redirect("/work");
 })
 
 app.post("/", function (req, res) {
-    var item = req.body.newItem;
-    items.push(item);
 
-    res.redirect("/");
-})
+    var item = req.body.newItem;
+
+    if (req.body.list === "Work List") {
+        workList.push(item);
+        res.redirect("/work");
+    }
+    else {
+
+        items.push(item);
+        res.redirect("/");
+    }
+
+});
 
 app.listen(3300, function () {
     console.log("LIstening at port number 3300.");
-})
+});
--- tp.js
+++
@@ -1,3 +0,0 @@
-<%  for (let i = 0; i < newListItems.length; i++) {  %>
-    <li><%= newListItems[i] %></li>
-<% } %>
